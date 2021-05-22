cf5ec9c0d61a0ad5be4f66f1991f875941c1dc87
Anderson Parra <parraanderson521@gmail.com>
2021-05-21 22:45:08 -0500

    Talller Guiado del Buscaminas en Visual Basic

---
+++ BuscaMinas/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NumericUpDownAlto = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDownAncho = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDownMinas = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ButtonCargar = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDownAlto, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDownAncho, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDownMinas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NumericUpDownAlto
+        '
+        Me.NumericUpDownAlto.Location = New System.Drawing.Point(59, 28)
+        Me.NumericUpDownAlto.Name = "NumericUpDownAlto"
+        Me.NumericUpDownAlto.Size = New System.Drawing.Size(104, 20)
+        Me.NumericUpDownAlto.TabIndex = 0
+        Me.NumericUpDownAlto.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NumericUpDownAncho
+        '
+        Me.NumericUpDownAncho.Location = New System.Drawing.Point(293, 28)
+        Me.NumericUpDownAncho.Name = "NumericUpDownAncho"
+        Me.NumericUpDownAncho.Size = New System.Drawing.Size(105, 20)
+        Me.NumericUpDownAncho.TabIndex = 1
+        Me.NumericUpDownAncho.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NumericUpDownMinas
+        '
+        Me.NumericUpDownMinas.Location = New System.Drawing.Point(490, 28)
+        Me.NumericUpDownMinas.Name = "NumericUpDownMinas"
+        Me.NumericUpDownMinas.Size = New System.Drawing.Size(106, 20)
+        Me.NumericUpDownMinas.TabIndex = 2
+        Me.NumericUpDownMinas.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Alto"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(445, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Minas"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(248, 30)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Ancho"
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoSize = True
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Location = New System.Drawing.Point(59, 79)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(659, 359)
+        Me.Panel1.TabIndex = 4
+        '
+        'ButtonCargar
+        '
+        Me.ButtonCargar.Location = New System.Drawing.Point(643, 30)
+        Me.ButtonCargar.Name = "ButtonCargar"
+        Me.ButtonCargar.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonCargar.TabIndex = 0
+        Me.ButtonCargar.Text = "Cargar"
+        Me.ButtonCargar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonCargar)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDownMinas)
+        Me.Controls.Add(Me.NumericUpDownAncho)
+        Me.Controls.Add(Me.NumericUpDownAlto)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Buscaminas 2021 Anderson Yeisd Parra Florez"
+        CType(Me.NumericUpDownAlto, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDownAncho, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDownMinas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents NumericUpDownAlto As NumericUpDown
+    Friend WithEvents NumericUpDownAncho As NumericUpDown
+    Friend WithEvents NumericUpDownMinas As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ButtonCargar As Button
+End Class
---
+++ BuscaMinas/Form1.vb
@@ -0,0 +1,199 @@
+﻿Public Class Form1
+
+    Dim Minas, TamañoX, TamañoY As Integer
+    Dim Mapa(,) As Integer
+    Dim Estado(,) As Boolean
+    Dim AnchoCelda, AltoCelda As Integer
+    Dim Entro As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        TamañoX = NumericUpDownAlto.Value
+        TamañoY = NumericUpDownAncho.Value
+        Minas = NumericUpDownMinas.Value
+        ReDim Mapa(TamañoX, TamañoY)
+        ReDim Estado(TamañoX, TamañoY)
+        Entro = "Vacio"
+        AnchoCelda = 25
+        AltoCelda = 25
+
+    End Sub
+
+    Dim Posiciones As PointF
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+        e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias
+        Posiciones = New PointF(10, 10)
+        Cargar(e.Graphics)
+    End Sub
+
+    Dim CeldaX, CeldaY As Integer
+    Function Cargar(g As Graphics)
+
+        Dim Brush As Brush = Brushes.DarkGray
+        Dim lapizNegro As New Pen(Color.Black, 1.0F)
+        'vector
+        If Entro = "Vacio" Then
+            For x As Integer = 0 To TamañoX - 1 Step 1
+                For y As Integer = 0 To TamañoY - 1 Step 1
+                    Mapa(x, y) = 0
+                    Estado(x, y) = False
+                    g.FillRectangle(Brush, x * AnchoCelda + Posiciones.X, y *
+                                    AltoCelda + Posiciones.Y, AnchoCelda, AltoCelda)
+                Next
+            Next
+            PonerMinas()
+            'Entro = "Muerto"
+            MostrarTodo(g)
+        ElseIf Entro = "Validar" Then
+            Dim Fila, Columna As Integer
+            If Estado(CeldaX, CeldaY) = False Then
+                Estado(CeldaX, CeldaY) = True
+                Dim Valor As Integer = Mapa(celdaX, CeldaY)
+                If Mapa(CeldaX, CeldaY) = 9 Then
+                    Entro = "Muerto"
+                ElseIf Mapa(CeldaX, CeldaY) = 0 Then
+                    For Fila = Max(0, CeldaX - 1) To Min(TamañoX, CeldaX + 1) Step 1
+                        For Columna = Max(0, CeldaY - 1) To Min(TamañoY, CeldaY + 1) Step 1
+
+                            If Mapa(Fila, Columna) <> 9 Then
+                                Estado(Fila, Columna) = True
+                            End If
+                        Next
+                    Next
+                End If
+            End If
+            MostrarTodo(g)
+        End If
+        'Lineas horizontales
+        For i As Integer = 0 To TamañoY Step 1
+            g.DrawLine(lapizNegro, Posiciones.X, AltoCelda * i + Posiciones.Y,
+        AnchoCelda * TamañoX + Posiciones.X, AltoCelda * i + Posiciones.Y)
+        Next
+        'Lineas verticales
+        For i As Integer = 0 To TamañoX Step 1
+            g.DrawLine(lapizNegro, AnchoCelda * i + Posiciones.X, Posiciones.Y,
+        AnchoCelda * i + Posiciones.X, AltoCelda * TamañoY + Posiciones.Y)
+        Next
+        Return ""
+    End Function
+
+    Function PonerMinas()
+        Dim Recorrido, Fila, Columna, Fila2, Columna2, Final As Integer
+        Dim Aleatorio As Random = New Random
+        Recorrido = 1
+        Final = Minas
+        While Recorrido <= Final
+            Do
+                Fila = Aleatorio.Next(0, TamañoX - 1)
+                Columna = Aleatorio.Next(0, TamañoY - 1)
+            Loop Until (Mapa(Fila, Columna) <> 9)
+            'Reperesentamos la mina con el nùmero 9
+            Mapa(Fila, Columna) = 9
+
+            'Ahora incremento el nùmero de minas cercanas en las casillas vecinas
+            For Fila2 = Max(0, Fila - 1) To Min(TamañoX, Fila + 1) Step 1
+                For Columna2 = Max(0, Columna - 1) To Min(TamañoY, Columna + 1) Step 1
+                    'le crea los numeros a la matriz para su anàlisis
+                    If Mapa(Fila2, Columna2) <> 9 Then
+                        Mapa(Fila2, Columna2) = Mapa(Fila2, Columna2) + 1
+                    End If
+                Next
+            Next
+            Recorrido += 1
+
+        End While
+        Return ""
+    End Function
+
+    Function MostrarTodo(g As Graphics)
+        Dim Brush As Brush = Brushes.White
+        Dim brushMina As Brush = Brushes.Red
+        Dim brushTexto As Brush = Brushes.Black
+        'Casillas
+        For x As Integer = 0 To TamañoX - 1 Step 1
+            For y As Integer = 0 To TamañoY - 1 Step 1
+                Dim Valor As Integer = Mapa(x, y)
+                Dim Condicion As Boolean = Estado(x, y)
+                Select Case Entro
+                    Case "Validar"
+                        Select Case Condicion
+                            Case True
+                                g.FillRectangle(Brush, x * AnchoCelda + Posiciones.X, y * AltoCelda + Posiciones.Y, AnchoCelda, AltoCelda)
+                                If Valor = 0 Then
+                                    g.DrawString("", New Font("Arial", 13), brushTexto, x * AnchoCelda + Posiciones.X, y * AltoCelda + Posiciones.Y)
+                                ElseIf Valor <> 9 Then
+                                    g.FillRectangle(Brush, x * AnchoCelda + Posiciones.X, y * AltoCelda + Posiciones.Y, AnchoCelda, AltoCelda)
+                                    g.DrawString(Valor.ToString(), New Font("Arial", 13), brushTexto, x * AnchoCelda + Posiciones.X, y * AltoCelda + Posiciones.Y)
+                                End If
+                        End Select
+                    Case "Muerto"
+                        Select Case Valor
+                            Case 0
+                                g.FillRectangle(Brush, x * AnchoCelda + Posiciones.X, y * AltoCelda + Posiciones.Y, AnchoCelda, AltoCelda)
+                            Case 9
+                                g.FillRectangle(brushMina, x * AnchoCelda + Posiciones.X, y * AltoCelda + Posiciones.Y, AnchoCelda, AltoCelda)
+                            Case Else
+                                g.DrawString(Valor.ToString(), New Font("Arial", 13), brushTexto, x * AnchoCelda + Posiciones.X, y * AltoCelda + Posiciones.Y)
+                        End Select
+                        Estado(x, y) = True
+                End Select
+            Next
+        Next
+        If Entro = "Muerto" Then MsgBox("Perdio")
+        Return ""
+    End Function
+
+    Function Max(Numero1 As Integer, Numero2 As Integer) As Integer
+        Dim num As Integer
+        If Numero1 > Numero2 Then
+            num = Numero1
+        Else
+            num = Numero2
+        End If
+        Return num
+    End Function
+
+    Function Min(Numero1 As Integer, Numero2 As Integer) As Integer
+        Dim num As Integer
+        If Numero1 < Numero2 Then
+            num = Numero1
+        Else
+            num = Numero2
+        End If
+        Return num
+    End Function
+
+    Private Sub ButtonCargar_Click(sender As Object, e As EventArgs) Handles ButtonCargar.Click
+        If NumericUpDownMinas.Value < (NumericUpDownAlto.Value * NumericUpDownAncho.Value) - 1 Then
+            TamañoX = NumericUpDownAlto.Value
+            TamañoY = NumericUpDownAncho.Value
+            Minas = NumericUpDownMinas.Value
+            ReDim Mapa(TamañoX, TamañoY)
+            ReDim Estado(TamañoX, TamañoY)
+            Entro = "Vacio"
+            Panel1.Invalidate()
+        Else
+            MsgBox("Debe haber al menos una casilla sin minas!", vbCritical, "Advertencia")
+
+        End If
+    End Sub
+    Private Sub Panel1_MouseClick(sender As Object, e As MouseEventArgs) Handles Panel1.MouseClick
+
+        CeldaX = (e.X - CInt(Posiciones.X) * (1 + 0.9)) / AnchoCelda
+        CeldaY = (e.Y - CInt(Posiciones.Y) * (1 + 0.9)) / AltoCelda
+        If Entro <> "Muerto" Then
+            If Estado(CeldaX, CeldaY) = False Then
+                Entro = "Validar"
+                Panel1.Invalidate()
+            Else
+                MsgBox(" Ya le dio Click a este ")
+            End If
+        Else
+            MsgBox(" Ya està muerto, perdiò ")
+        End If
+
+    End Sub
+
+
+End Class
